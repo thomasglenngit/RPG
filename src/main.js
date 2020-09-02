eb8ac72a525cbb9da0a5d8839f7f8e94a114cae5
Thomas Glenn <thomasgrantglenn@mac.com>
2020-09-02 12:36:34 -0600

    add changeState to main.js.
    Co-authored-by: Hannah <hannah.beinstein@gmail.com>
    Co-authored-by: hubba180 <kyle.james.hubbard@gmail.com>

--- src/main.js
+++ src/main.js
@@ -1,31 +1,79 @@
-import { MovieService } from './movies-service.js';
-import $ from 'jquery';
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './styles.css';
+// Jocks, Bangers, Nerds, Preps, Dweebs, Barbies, Goths
+
+// Intelligence, Attraction, Coolness, Hygiene, Sweetheart: {Dweebs, Bangers}
+
+// Battle Strategies: Avoidance, Rumble, Verbal, Detention, Pranks
+
+// Level-up: Popularity
+
+// sample character State: jock, name: harry potter intelligence: 2, Attraction: 10, Coolness: 8, Hygiene: 6, Sweetheart: {}
+// FootBallTeam = changeState() --- where change state updates attraction, coolness, maybe hygiene by 10 - 10 - 6 respectively
+// CheerleadTeam = cheerleadState()
+// Robotics = changeState()
+// DebateTeam = changeState
+// footballjock = jock(FootballTeam)
+
+// let Jock = { Intelligence: 1, Attraction: 20, Coolness: 15, Hygiene: 10 }
+// let Banger = { Intelligence: 4, Attraction: 8, Coolness: 14, Hygiene: 7 }
+// let Nerd = { Intelligence: 20, Attraction: 4, Coolness: 1, Hygiene: 7 }
+// let Prep = { Intelligence: 10, Attraction: 15, Coolness: 8, Hygiene: 20 }
+// let Dweeb = { Intelligence: 1, Attraction: 0, Coolness: 0, Hygiene: 0 }
+// let Barbie = { Intelligence: 7, Attraction: 20, Coolness: 15, Hygiene: 10 }
+// let Goth = { Intelligence: 10, Attraction: 5, Coolness: 8, Hygiene: 7 }
 
 
-$(document).ready(function() {
+// const addAdditionalStateChange = (prop) => {
+//   return (value) => {
+//     return (state) => ({
+//       ...state,
+//       [prop] : (state [prop] || 0) + value
+//     })
+//   }
+// }
 
-  $('#movieSelection').click(function() {
-    const movie = $('#movieSearch').val();
-    $('#movieSearch').val("");
+let Template = { Intelligence: 0, Attraction: 0, Coolness: 0, Hygiene: 0 }
 
-    (async () => {
-      let movieService = new MovieService();
-      const response = await movieService.get(movie);
-      getElements(response);
-    })();
+const storeState = (initialState = {}) => {
+  let currentState = initialState;
+  return (stateChangeFunction = state => state) => {
+    const newState = stateChangeFunction(currentState);
+    currentState = {...newState};
+    return newState;
+  }
+}
 
-    function getElements(response) {
-      if (response) {
-        $('.list').append(`"<li>" The official title of ${movie} is ${response.results[0].title}"</li>"`);
-        $('.list').append(`"<li>" Here's what it's about: ${response.results[0].overview} "<li>"`);
-      } else {
-        $('.list').append(`"<li>" No movies for you!"</li>"`);
-        $('.list').append(`"<li>" Please check your inputs and try again! "</li>"`);
+const changeState = (prop) => {
+  return (value) => {
+    return (state) => ({
+      ...state,
+      [prop] : (state[prop] || 0) + value
+      
+    })
+  }
+}
+
+const changeState = (intelNum) => {
+  return (attractionNum) => {
+    return (coolnessNum) => {
+      return (hygieneNum) => {
+        return (state) => ({
+          ...state,
+          Intelligence: (state[Intelligence] || 0) + intelNum, 
+          Attraction: (state[Attraction] || 0) + attractionNum,
+          Coolness: (state[Coolness] || 0) + coolnessNum,
+          Hygiene: (state[Hygenie] || 0) + hygieneNum 
+        })
       }
     }
+  }
+}
 
-  });
-});
+jockTemplate = changeState(10)(45)
+const Character = storeState(Template);
+const updatePlayer = changeState("playerName")
+
+// const jockCHaracter = Character(jockTemplate)
+
+
+
+
