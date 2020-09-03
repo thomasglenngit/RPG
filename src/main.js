544ab6dff82786bddd1e237d3c8d57ddd2f15a78
Thomas Glenn <thomasgrantglenn@mac.com>
2020-09-03 11:20:39 -0600

    add add remove sweetheart functions.
    Co-authored-by: Hannah <hannah.beinstein@gmail.com>
    Co-authored-by: hubba180 <kyle.james.hubbard@gmail.com>

--- src/main.js
+++ src/main.js
@@ -44,6 +44,33 @@ const storeState = (initialState = {}) => {
 //   }
 // }
 
+const levelUp = (newStateObj) => {
+  return (currentState) => { return ({
+    ...currentState,
+    ...newStateObj
+  }) }
+}
+
+const addSweetheart = (Sweetheart) => {
+  return (currentState) => { return ({
+    Intelligence: Sweetheart.Intelligence + currentState.Intelligence,
+    Attraction: Sweetheart.Attraction + currentState.Attraction,
+    Coolness: Sweetheart.Coolness + currentState.Coolness,
+    Hygiene: Sweetheart.Hygiene + currentState.Hygiene,
+    Sweetheart: { ...Sweetheart}
+  }) }
+}
+
+const breakupSweetheart = (currentState) => { return ({
+    Intelligence: currentState.Intelligence - currentState.Sweetheart.Intelligence,
+    Attraction: currentState.Attraction - currentState.Sweetheart.Attraction,
+    Coolness: currentState.Coolness - currentState.Sweetheart.Coolness,
+    Hygiene: currentState.Hygiene - currentState.Sweetheart.Hygiene
+  }) 
+}
+
+
+
 const changeState = (intelNum) => {
   return (attractionNum) => {
     return (coolnessNum) => {
@@ -77,22 +104,35 @@ const prepTemplate = changeState(10)(15)(8)(20);
 const dweebTemplate = changeState(1)(0)(0)(0);
 const barbieTemplate = changeState(7)(20)(15)(10);
 const gothTemplate = changeState(10)(5)(8)(7);
-const joinFootball = changeState(100)(100)(100)(100);
+
+const joinFootball = changeState(0)(15)(40)(-20);
 const joinMetalShop = changeState(20)(5)(20)(-8);
+const joinChessClub = changeState(30)(-10)(-10)(5);
+const joinLaCrosse = changeState(5)(10)(10)(-5);
+const joinDandD = changeState(0)(-20)(-10)(-2);
+const joinCheer = changeState(0)(40)(20)(0);
+const joinMetalBand = changeState(8)(5)(-10)(0);
 
 //Character template assignments:
-const jockCharacter = CharacterJock(jockTemplate);
-const bangerCharacter = CharacterBanger(bangerTemplate);
-const nerdCharacter = CharacterNerd(nerdTemplate);
-const prepCharacter = CharacterPrep(prepTemplate);
-const dweebCharacter = CharacterDweeb(dweebTemplate);
+const jockCharacter = CharacterJock(jockTemplate); 
 const barbieCharacter = CharacterBarbie(barbieTemplate);
+const prepCharacter = CharacterPrep(prepTemplate);
+const bangerCharacter = CharacterBanger(bangerTemplate);
 const gothCharacter = CharacterGoth(gothTemplate);
+const nerdCharacter = CharacterNerd(nerdTemplate);
+const dweebCharacter = CharacterDweeb(dweebTemplate);
 
 //Level up opportunities:
 const FootballJock = CharacterJock(joinFootball);
 const nerd = CharacterNerd(joinFootball);
-console.log(nerd); 
+const jockboyfriend = addSweetheart(prepCharacter)(jockCharacter);
+console.log(jockboyfriend);
+const heartBrokenJock = breakupSweetheart(jockboyfriend);
+console.log(heartBrokenJock);
+
+
+// 1st: { Intelligence: 11 Attraction: 35 Coolness: 23 Hygiene: 30 SweetHeart: {a;dslfj}}
+// 2nd: { Intelligence: 1 Attraction: 20 Coolness: 15 Hygiene: 10 }
 
 // jockCharacter(Character(storeState(currentState)))
 // jockCharacter = storestate()(jocktemplate)
